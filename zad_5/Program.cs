5528910a8f7562f5e323e73698ad77840916e125
Alex <Yulien.ru@Yandex.ru>
2022-08-27 09:42:19 +0400

    есть решение задачи _выведение последнего числа у трехзначного числа_

--- zad_5/Program.cs
+++ zad_5/Program.cs
@@ -1,6 +1,15 @@
-﻿System.Console.WriteLine();
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
 
-int number = new Random().Next(100, 1000);
+// 1 вариант с Рандомным числом
+// System.Console.WriteLine();
 
-System.Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}");
-System.Console.WriteLine();
+// int number = new Random().Next(100, 1000);
+
+// System.Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}");
+// System.Console.WriteLine();
+
+// 2 вариант с Введением числа вручную из семинара
+
+Console.WriteLine ("Введите 3-х значное число: ");
+int threeN = int.Parse (Console.ReadLine());
+Console.WriteLine ($"Последняя цифра числа {threeN} - " + threeN % 10); // + здесь отвечает за присоединение
