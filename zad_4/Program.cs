533e5b6a1ffad738e9469416a2af434d32b624c1
Alex <Yulien.ru@Yandex.ru>
2022-08-27 09:32:31 +0400

    есть решение задачи _целые числа от -N до N_

--- zad_4/Program.cs
+++ zad_4/Program.cs
@@ -1,12 +1,24 @@
-﻿System.Console.Write("Введите число:   ");
-string writeNumber = Console.ReadLine();
-int endNumber = Convert.ToInt32(writeNumber);
-int Number = endNumber * (-1);
-System.Console.Write($"{Number} ");
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
 
-while (Number < endNumber)
+// System.Console.Write("Введите число:   ");
+// string N = Console.ReadLine();
+// int endNumber = Convert.ToInt32(N);
+// int Number = endNumber * (-1); // умножаем на отрицательную единицу, чтобы отсчет пошел с минус N
+// System.Console.Write($"{Number} ");
+
+// while (Number < endNumber)
+// {
+//     Number++;
+//     System.Console.Write($"{Number} ");
+// }
+// System.Console.WriteLine(); System.Console.WriteLine();
+
+// В семинаре задача решена проще.
+
+Console.WriteLine ("Введите число");
+int N = int.Parse (Console.ReadLine());
+
+for (int i = -N; i < N + 1; i++)
 {
-    Number++;
-    System.Console.Write($"{Number} ");
+    Console.Write(i + " ");
 }
-System.Console.WriteLine(); System.Console.WriteLine();
